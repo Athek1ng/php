c8bf9827d5bd36ab9a36a5a8891ddd494a8ffdee
Athek1ng <arthurpascoalb@gmail.com>
2026-08-28 21:53:34 -0300

    Add files via upload

---
+++ arthur pascoal - php/php_aulas/3_Tipos_de_dados/2_Exercicio_3/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Números inteiros</title>
+</head>
+<body>
+    
+<?php
+echo 10;
+echo "<br>";
+echo 14;
+echo "<br>";
+echo 78;
+echo "<br>";
+echo 245;
+echo "<br>";
+echo 1455;
+echo "<br>";
+
+?>
+</body>
+</html>
